6e399f1e93971d6cd6b6a06d3e5a98158241e210
Wesley <wesley_@live.nl>
2017-03-03 13:35:10 +0100

    fotos

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/index' , to: "static_pages#index", as: "index"
-  get 'static_pages/appartementen' , to: "static_pages#appartementen", as: "appartementen"
-  get 'static_pages/reserveren' , to: "static_pages#reserveren", as: "reserveren"
-  get 'static_pages/amelanderkaap', to: "static_pages#amelanderkaap", as: "amelanderkaap"
-  get 'static_pages/contact', to: "static_pages#contact", as: "contact"
+  get 'index' , to: "static_pages#index", as: "index"
+  get 'appartementen' , to: "static_pages#appartementen", as: "appartementen"
+  get 'reserveren' , to: "static_pages#reserveren", as: "reserveren"
+  get 'amelanderkaap', to: "static_pages#amelanderkaap", as: "amelanderkaap"
+  get 'contact', to: "static_pages#contact", as: "contact"
 
   root 'static_pages#index'
 
